5aa8790088cd6bb598c19bfe0c6397c7c05b3264
khanh.ha <hakhanh.1106@gmail.com>
2021-04-04 15:02:06 +0700

    Fix switch darkmode

--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -3,13 +3,13 @@ import { ReactComponent as SunnyIcon } from "assets/images/sunny.svg";
 import { ReactComponent as NightIcon } from "assets/images/night.svg";
 import { SwitchWrapper } from "./styles";
 
-function ToggleSwitch({ onSwitch }) {
+function ToggleSwitch({ onSwitch, isToggle }) {
   function onSwitchClicked() {
     onSwitch();
   }
 
   return (
-    <SwitchWrapper onClick={onSwitchClicked} data-testid="toggle-switch">
+    <SwitchWrapper onClick={onSwitchClicked} data-testid="toggle-switch" isToggle={isToggle}>
       <ToggleIcon />
       <SunnyIcon />
       <NightIcon />
